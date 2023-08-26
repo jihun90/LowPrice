6610323e0a76db8c658b27f6b946cec4752644ac
jihun90 <jihun90.kim@icloud.com>
2023-08-26 10:24:31 +0900

    Product에 약간의 css 적용

--- app/apple/compents/product.tsx
+++ app/apple/compents/product.tsx
@@ -1,12 +1,11 @@
+type prop = { name: string; image?: string; price: number; bestPrice?: number };
 
-
-type prop = { name : string, price : number }
-
-export default function Product(prop : prop)
-{
+export default function Product(prop: prop) {
     return (
-    <ul>
-        <li>{prop.name}</li>
-        <li>{prop.price}</li>
-    </ul>)
+        <div className='flex flex-row'>
+            <div className='inline-flex basis-20'>사진</div>
+            <div className='inline-flex basis-full'>{prop.name}</div>
+            <div className='inline-flex basis-60'>{prop.price}</div>
+        </div>
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import './globals.css';
 import Link from 'next/link';
 
 import type { Metadata } from 'next';
