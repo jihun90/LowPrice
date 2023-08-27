3b38157713418db5fdc0cca9b5a3a5ae6cde1423
jihun90 <jihun90.kim@icloud.com>
2023-08-27 09:39:42 +0900

    css 수정

--- app/apple/compents/product.tsx
+++ app/apple/compents/product.tsx
@@ -1,19 +1,20 @@
 import Image from 'next/image';
 
-type prop = { name: string; image?: string; price: number; bestPrice?: number };
+type prop = { name: string; image?: string; price: number };
 
 export default function Product(prop: prop) {
     return (
-        <div className='flex flex-row'>
-            <Image
-                className='inline-flex basis-20 align-middle'
-                alt=''
-                src={prop.image ?? ''}
-                width={100}
-                height={100}
-            />
-            <div className='inline-flex basis-full'>{prop.name}</div>
-            <div className='inline-flex basis-60'>{prop.price}</div>
+        <div className='flex flex-row my-1 p-2'>
+            <div className='inline-flex basis-20 border-2 border-solid border-indigo-300'>
+                <Image
+                    alt={prop.name}
+                    src={prop.image ?? ''}
+                    width={150}
+                    height={150}
+                />
+            </div>
+            <div className='inline-flex basis-full p-2'>{prop.name}</div>
+            <div className='inline-flex basis-60 p-2'>{prop.price}</div>
         </div>
     );
 }
