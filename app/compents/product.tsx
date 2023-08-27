9a206702391eb9496f790b05f8619381734586cc
jihun90 <jihun90.kim@icloud.com>
2023-08-27 18:31:42 +0900

    css 수정

--- app/compents/product.tsx
+++ app/compents/product.tsx
@@ -6,8 +6,8 @@ type prop = { name: string; image?: string; price: number; url: string };
 export default async function Product(prop: prop) {
     return (
         <Link href={prop.url}>
-            <div className='flex flex-row my-1 p-2'>
-                <div className='inline-flex basis-20 border-2 border-solid border-indigo-300'>
+            <div className='flex flex-row p-2 hover:bg-slate-100'>
+                <div className='inline-flex basis-32 align-middle'>
                     <Image
                         alt={prop.name}
                         src={prop.image ?? ''}
@@ -16,7 +16,7 @@ export default async function Product(prop: prop) {
                     />
                 </div>
                 <div className='inline-flex basis-full p-2'>{prop.name}</div>
-                <div className='basis-60 p-2'>
+                <div className='basis-48 p-2'>
                     <div className='text-sm text-center'>{getToday()}</div>
                     <div className='text-center underline font-bold'>{`${prop.price.toLocaleString(
                         'ko-KR'
--- app/compents/productListview.tsx
+++ app/compents/productListview.tsx
@@ -7,17 +7,21 @@ type props = { title: string; productList: SearchProduct[] };
 export default function ProductListView(props: props) {
     return (
         <div className='my-3 p-3'>
-            <h2 className='text-xl font-semibold'>{props.title}</h2>
+            <h2 className='text-xl font-semibold'> {props.title}</h2>
             <div>
-                {props.productList.map((product) => (
-                    <Product
-                        key={product.productId}
-                        name={product.productName}
-                        price={product.productPrice}
-                        image={product.productImage}
-                        url={product.productUrl}
-                    />
-                ))}
+                <ul role='list'>
+                    {props.productList.map((product) => (
+                        <li key={product.productId} className='py-2'>
+                            <Product
+                                key={product.productId}
+                                name={product.productName}
+                                price={product.productPrice}
+                                image={product.productImage}
+                                url={product.productUrl}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
