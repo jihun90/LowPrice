312400b8fc6200e7195357eac9e93ef28cbd0296
jihun90 <jihun90.kim@icloud.com>
2023-08-27 18:49:00 +0900

    footer css 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
                 <Link href='/'>Low Price</Link>
             </h1>
             <>{children}</>
-            <h5 className='text-center'>
+            <h5 className='my-8 text-center align-middle text-sm text-gray-400'>
                 이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의
                 수수료를 제공받을 수 있습니다.
             </h5>
