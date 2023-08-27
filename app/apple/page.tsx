369f5c8247ed4806ee4f2c7521d49ecc21535144
jihun90 <jihun90.kim@icloud.com>
2023-08-27 10:03:05 +0900

    키워드랑 제목이 정확히 일치할때만 보여주도록 수정(임시)

--- app/apple/page.tsx
+++ app/apple/page.tsx
@@ -7,23 +7,26 @@ import type { SearchProduct } from '@/api/responseModel';
 
 export default async function Apple() {
     const request: SearchProductRequest = {
-        keyword: '가전디지털 아이폰14 자급제',
+        keyword: 'Apple 정품 아이폰 14 자급제',
         imageSize: '150x150',
     };
 
     let productList: SearchProduct[] = [];
     productList = await searchProduct(request);
-    console.log(productList);
+
     return (
         <div>
-            {productList.map((product) => (
-                <Product
-                    key={product.productId}
-                    name={product.productName}
-                    price={product.productPrice}
-                    image={product.productImage}
-                />
-            ))}
+            {productList.map(
+                (product) =>
+                    product.productName === request.keyword && (
+                        <Product
+                            key={product.productId}
+                            name={product.productName}
+                            price={product.productPrice}
+                            image={product.productImage}
+                        />
+                    )
+            )}
         </div>
     );
 }
